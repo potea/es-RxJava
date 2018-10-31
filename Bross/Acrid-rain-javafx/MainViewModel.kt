44bcdb699b97844ea651c36ded527fc989a905f6
Bross <rkd2468@gmail.com>
2018-10-31 15:32:06 +0900

    Update MainViewModel.kt

--- Bross/Acrid-rain-javafx/MainViewModel.kt
+++ Bross/Acrid-rain-javafx/MainViewModel.kt
@@ -1,3 +1,21 @@
+import io.reactivex.Observable
+import io.reactivex.Scheduler
+import io.reactivex.rxjavafx.observables.JavaFxObservable
+import io.reactivex.rxjavafx.schedulers.JavaFxScheduler
+import io.reactivex.rxkotlin.zipWith
+import io.reactivex.schedulers.Schedulers
+import javafx.beans.property.*
+import javafx.collections.FXCollections
+import javafx.event.ActionEvent
+import javafx.geometry.Pos
+import javafx.scene.Node
+import javafx.scene.control.Label
+import javafx.scene.control.SplitPane
+import javafx.scene.layout.HBox
+import javafx.scene.layout.Pane
+import views.VerticalMoveLabel
+import java.util.concurrent.TimeUnit
+
 
 infix fun ClosedRange<Double>.step(step: Double): Iterable<Double> {
     require(start.isFinite())
@@ -21,72 +39,45 @@ class MainViewModel {
     val currentInput = SimpleStringProperty()
     val countString = ReadOnlyStringWrapper()
 
-    val correctString = ReadOnlyStringWrapper()
-
     lateinit var enterEvents: Observable<ActionEvent>
     val containerObservable = FXCollections.observableArrayList<HBox>((1..MAX_CONTAINER).map { HBox() })
     val stageObservable = Observable.just(containerObservable).flatMapIterable { it -> it }.map {
         it.alignment = Pos.TOP_CENTER
         it
-    }.repeat()
-    val speed = Observable.just((5..100).step(5)).flatMapIterable { it -> it }.repeat()
-    val labelObservable = Observable.just(Label(worModel.sampleWord()))
-    val labelY = ReadOnlyDoubleWrapper()
-    val speedWrapper = ReadOnlyIntegerWrapper()
+    }
 
-
-    val paneWrapper = ReadOnlyObjectWrapper<HBox>()
-
-    private val INTERVAL_TIME: Long = 1 // seconds
-    val trigger = Observable.interval(INTERVAL_TIME, TimeUnit.SECONDS)
+    val speed = ReadOnlyIntegerWrapper(5)
+    val labels = mutableListOf<VerticalMoveLabel>()
 
     init {
         countString.bind(count.asString())
 
-        var stage = stageObservable.filter { it.children.isEmpty() }.blockingFirst()
-        speedWrapper.value = speedSample()
-
-        paneWrapper.value = stage
-
-        labelObservable
-                .map {
-                    labelY.bindBidirectional(it.translateYProperty())
-                    it.translateY += speedWrapper.readOnlyProperty.value
-                    it
-                }
-                .zipWith(trigger)
-                .repeat()
-                .observeOn(JavaFxScheduler.platform())
+        stageObservable
+                .filter { it.children.isEmpty() }
                 .subscribe {
-                    //                    val stage = stageSample()
-                    val label = it.first
-                    correctString.bindBidirectional(label.textProperty())
-                    stage.children.clear()
-                    if (stage.height >= label.translateY) {
-                        stage.children.add(label)
-                    } else {
-                        speedWrapper.value = speedSample()
-                        label.text = worModel.sampleWord()
-                        label.translateY = 0.0
-                        stage = stageSample()
-                    }
+                    val label = VerticalMoveLabel(worModel.sampleWord(), speed.value)
+                    it.children.add(label)
+                    label.move()
+                    labels.add(label)
                 }
     }
 
     fun bindEnterEvent(node: Node) {
         enterEvents = JavaFxObservable.actionEventsOf(node)
-        enterEvents.subscribe {
+        enterEvents.subscribe { _ ->
 
-            if (correctString.value == currentInput.value) {
-                count.value += correctString.value.length
-                correctString.value = ""
-                labelY.value = 0.0
-                correctString.value = worModel.sampleWord()
-                speedWrapper.value = speedSample()
-                paneWrapper.value = stageSample()
-            }
+            labels.asSequence()
+                    .filter { it.isCorrect(currentInput.value) }
+                    .maxBy { it.positionY.value }
+                    .let {
+                        if (it != null) {
+                            count.value += it.text.length
+                            speed.value += 1
+                            it.invalidate(spped = speed.value)
+                            it.text = worModel.sampleWord()
+                        }
+                    }
             currentInput.value = ""
-
         }
     }
 
@@ -95,13 +86,4 @@ class MainViewModel {
         val increment = (1 / containerObservable.size.toDouble())
         container.setDividerPositions(*increment.rangeTo(1.0).step(increment).map { Math.round(it * 100) / 100.0 }.toDoubleArray())
     }
-
-    fun stageSample(): HBox {
-        return stageObservable.filter { it.children.isEmpty() }.sample(1, TimeUnit.MICROSECONDS).blockingFirst()
-    }
-
-    fun speedSample(): Int {
-        return speed.sample(1, TimeUnit.MICROSECONDS).blockingFirst()
-    }
-
 }
