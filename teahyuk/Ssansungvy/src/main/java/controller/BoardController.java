ef5f7285a53461d9ff2be4098aade4f106a3c940
teahyuk <jtaeh57@gmail.com>
2018-12-01 03:58:19 +0900

    산성비 2차개발 완료.

--- teahyuk/Ssansungvy/src/main/java/controller/BoardController.java
+++ teahyuk/Ssansungvy/src/main/java/controller/BoardController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import io.reactivex.disposables.Disposable;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -9,8 +10,8 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import model.SentenceMaker;
 import model.SentencesManager;
+import model.vo.GameLevel;
 import model.vo.Sentence;
-import rx.Subscription;
 
 import java.net.URL;
 import java.util.*;
@@ -19,22 +20,33 @@ import java.util.stream.Collectors;
 
 public class BoardController implements Initializable {
 
+    private int LEVEL_UP_COUNT = 10;
+    private int removedCount = 0;
+
+    @FXML
+    private Label levelLabel;
+
+    @FXML
+    private Label lifeLabel;
+
+    @FXML
+    private Label scoreLabel;
+
     @FXML
     private AnchorPane pane;
 
-    private SentenceMaker sentenceMaker;
     private SentencesManager sentencesManager;
     private Map<Sentence, Node> labelMap;
     private Random random;
     private Double paneHeight;
     private Double paneWidth;
 
-    private Subscription makeSentenceSubscription;
+    private Disposable makeSentenceDisposable;
+
+    private GameLevel currentGameLevel;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        sentenceMaker = new SentenceMaker();
-
         random = new Random();
         labelMap = new HashMap<>();
 
@@ -42,18 +54,28 @@ public class BoardController implements Initializable {
         pane.widthProperty().addListener((observable, oldValue, newValue) -> paneWidth = (Double) newValue);
     }
 
-    public void startRaining(int maxIntervalMS, int minIntervalMS) {
-        sentencesManager = new SentencesManager();
-        sentencesManager.getSentencesHotObservable()
+    public void startRaining() {
+        currentGameLevel = new GameLevel();
+        scoreLabel.setText("0");
+        lifeLabel.setText("5");
+        this.startRaining(currentGameLevel);
+    }
+
+    public void startRaining(GameLevel level) {
+        currentGameLevel = level;
+        levelLabel.setText(String.valueOf(currentGameLevel.getLevel()));
+        SentenceMaker sentenceMaker = new SentenceMaker(level.getMaxInterval(), level.getMinInterval(), level.getMaxCharLength(), level.getMinCharLength());
+        sentencesManager = new SentencesManager(level.getMaxDropSpeed(), level.getMinDropSpeed());
+        Disposable disposable = sentencesManager.getSentencesObservable()
                 .buffer(100, TimeUnit.MICROSECONDS)
                 .subscribe(this::updateView,
-                        this::logging,
-                        this::gameOver);
+                        this::logging);
 
-        makeSentenceSubscription = sentenceMaker.getSentences(maxIntervalMS, minIntervalMS)
+        makeSentenceDisposable = sentenceMaker.getSentences()
                 .doOnNext(this::visibleSentence)
                 .subscribe(sentencesManager::addSentences,
-                        this::logging);
+                        this::logging,
+                        disposable::dispose);
     }
 
     private void visibleSentence(Sentence sentence) {
@@ -71,15 +93,34 @@ public class BoardController implements Initializable {
         Platform.runLater(() -> sentences
                 .stream()
                 .filter(labelMap::containsKey)
-                .forEach(sentence -> labelMap.get(sentence).setLayoutY(paneHeight * 0.95 * sentence.getHeight() / 1000)));
+                .forEach(this::updateSentence));
+    }
+
+    private void updateSentence(Sentence sentence) {
+        if (sentence.getHeight() == SentencesManager.SENTENCE_DIVIDE_LENGTH) {
+            removeSentence(sentence);
+            loseLife();
+        } else {
+            labelMap.get(sentence).setLayoutY(paneHeight * 0.95 * sentence.getHeight() / SentencesManager.SENTENCE_DIVIDE_LENGTH);
+        }
+    }
+
+    private void loseLife() {
+        int life = Integer.parseInt(lifeLabel.getText());
+        life--;
+        if (life <= 0)
+            gameOver();
+        else
+            lifeLabel.setText(String.valueOf(life));
     }
 
     private void gameOver() {
-        makeSentenceSubscription.unsubscribe();
+        makeSentenceDisposable.dispose();
         Platform.runLater(() -> {
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("게임오바아");
-            alert.setHeaderText("게임 오버되었습니다.");
+            alert.setHeaderText("게임 오버되었습니다. \n 처음부터 ?");
+            sentencesManager.close();
             alert.showAndWait();
             restart();
         });
@@ -90,14 +131,27 @@ public class BoardController implements Initializable {
         e.printStackTrace();
     }
 
-    public boolean removeSentence(String sentence) {
-        Sentence removed = sentencesManager.popSentence(sentence);
+    boolean removeSentence(String sentence) {
+        Sentence removed = sentencesManager.removeSentence(sentence);
         boolean removeSuccess = removed != null;
         if (removeSuccess) {
-            pane.getChildren().remove(labelMap.get(removed));
-            labelMap.remove(removed);
+            removeSentence(removed);
+
+            int score = Integer.parseInt(scoreLabel.getText());
+            score += removed.getSentence().length();
+            scoreLabel.setText(String.valueOf(score));
+            removedCount++;
+            if(removedCount>=LEVEL_UP_COUNT){
+                moveToNextLevel();
+                removedCount=0;
+            }
         }
         return removeSuccess;
+    }
+
+    private void removeSentence(Sentence removed) {
+        pane.getChildren().remove(labelMap.get(removed));
+        labelMap.remove(removed);
     }
 
     private void restart() {
@@ -106,6 +160,16 @@ public class BoardController implements Initializable {
                 .map(Sentence::getSentence)
                 .collect(Collectors.toList())
                 .forEach(this::removeSentence);
-        this.startRaining(1000, 500);
+        this.startRaining();
+    }
+
+    private void moveToNextLevel() {
+        labelMap.keySet()
+                .stream()
+                .collect(Collectors.toList())
+                .forEach(this::removeSentence);
+        makeSentenceDisposable.dispose();
+        sentencesManager.close();
+        startRaining(currentGameLevel.levelUp());
     }
 }
--- teahyuk/Ssansungvy/src/main/java/model/SentenceMaker.java
+++ teahyuk/Ssansungvy/src/main/java/model/SentenceMaker.java
@@ -1,26 +1,59 @@
 package model;
 
-import com.github.javafaker.Faker;
+import io.reactivex.Observable;
 import model.vo.Sentence;
-import rx.Observable;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class SentenceMaker {
 
-    public SentenceMaker() {
+    private List<String> sentences;
+    private Random random;
 
+    private int maxMakeInterval;
+    private int minMakeInterval;
+    private int maxLength;
+    private int minLength;
+
+    public SentenceMaker(int maxMakeInterval, int minMakeInterval, int maxLength, int minLength) {
+        try (BufferedReader br = Files.newBufferedReader(Paths.get("C:/Users/user/IdeaProjects/es-RxJava/teahyuk/Ssansungvy/target/classes/texts.csv"))){
+            sentences = Arrays.asList(br.readLine().split(","));
+            br.close();
+            random = new Random();
+            this.maxMakeInterval = maxMakeInterval;
+            this.minMakeInterval = minMakeInterval;
+            this.maxLength = maxLength;
+            this.minLength = minLength;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     //timeInterval ms
-    public Observable<Sentence> getSentences(int maxInterval, int minInterval) {
-        AtomicInteger interval = new AtomicInteger(maxInterval);
-        return Observable.defer(() ->
-                Observable.just(new Sentence(Faker.instance().dragonBall().character()))
-                        .delay(minInterval < interval.get() ? interval.getAndDecrement() : minInterval,
-                                TimeUnit.MILLISECONDS))
-                .repeat();
+    public Observable<Sentence> getSentences() {
+        if(sentences.size() == 0)
+            return Observable.error(new Exception("NoTextsException"));
+        AtomicInteger interval = new AtomicInteger(maxMakeInterval);
+        return Observable.defer(() -> Observable.just(new Sentence(sentences.get(random.nextInt(sentences.size())))))
+                .repeat()
+                .filter(this::isSentenceInLength)
+                .take(1)
+                .delay(minMakeInterval < interval.get() ? interval.getAndDecrement() : minMakeInterval,
+                                TimeUnit.MILLISECONDS)
+                .repeat()
+                .doOnDispose(()->System.out.println("dispose"));
+    }
+
+    private boolean isSentenceInLength(Sentence s){
+        return minLength <= s.getSentence().length() && s.getSentence().length() <= maxLength;
     }
 
 }
